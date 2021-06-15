d77949f595773b989bf18710da2a25149bdaef1c
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-06-15 13:56:37 +0100

    completed mobile styling except for underline and footer logo, working on hamburger functionality

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+// Variables
+const hamburgerIcon = document.querySelector('.icon-hamburger');
+
+
+
+// Functions
+function toggleDropdown() {
+    // get dropdown list
+    const dropdownList = document.querySelector('.dropdown-list');
+    if(dropdownList.style.display = 'none') {
+        dropdownList.style.display = 'block';
+    } else {
+        dropdownList.style.display = 'none'
+    }
+}
+
+
+
+
+
+// event listeners
+hamburgerIcon.addEventListener('click', toggleDropdown);
